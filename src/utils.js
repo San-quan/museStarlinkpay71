51cee956c0860f8466163e213763456b10e64906
San-quan <6069996@gmail.com>
2025-11-09 05:10:03 +0800

    fix: syntax error in template string

--- src/utils.js
+++ src/utils.js
@@ -14,7 +14,7 @@ function isValidIPv4 (ip) {
 }
 
 function greet (name = 'world') {
-  return `Hello, ${name}! This repo integrates with Starlink.'
+  return `Hello, ${name}! This repo integrates with Starlink.`
 }
 
 module.exports = {
